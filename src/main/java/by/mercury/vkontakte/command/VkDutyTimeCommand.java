68df4a54caeb3ce3b780fa8b70d20c257d9b9e69
Yegor-Ikbaev <yegor.ikbaev@gmail.com>
2020-04-30 16:52:29 +0300

    Add VkDutyTimeCommand

---
+++ src/main/java/by/mercury/vkontakte/command/VkDutyTimeCommand.java
@@ -0,0 +1,69 @@
+package by.mercury.vkontakte.command;
+
+import by.mercury.core.command.Command;
+import by.mercury.core.command.CommandContext;
+import by.mercury.core.data.MessageType;
+import by.mercury.core.model.MessageModel;
+import by.mercury.core.model.UserModel;
+import by.mercury.core.service.MessageService;
+import by.mercury.vkontakte.command.data.QueueNumberData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Slf4j
+@Component
+public class VkDutyTimeCommand implements Command {
+
+    private static final String MESSAGE_FORMAT = "Вы дежурный через %d недели";
+    private final List<String> keyWords = Arrays.asList("duty", "vigil", "daily", "order", "dude");
+
+    private MessageService messageService;
+
+    private String apiUrl;
+    
+    private RestTemplate restTemplate;
+
+    @Override
+    public boolean support(CommandContext context) {
+        return keyWords.stream()
+                .anyMatch(context.getMessage().getText().toLowerCase()::contains);
+    }
+
+    @Override
+    public void execute(CommandContext context) {
+        var message = MessageModel.builder()
+                .text(dutyTimeText(context.getMessage().getAuthor()))
+                .target(context.getMessage().getAuthor())
+                .types(Collections.singletonList(MessageType.VOICE))
+                .build();
+        messageService.send(message);
+    }
+
+
+    private String dutyTimeText(UserModel user) {
+        var queue = restTemplate.getForObject(apiUrl + user.getPeerId(), QueueNumberData.class).getQueueNumber();
+        return String.format(MESSAGE_FORMAT, queue - 1);
+    }
+
+    @Autowired
+    public void setMessageService(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @Autowired
+    public void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Value("${api.url}")
+    public void setApiUrl(String apiUrl) {
+        this.apiUrl = apiUrl;
+    }
+}
